75ab71a915dd94c219e941faec1fc534ab1ec9c2
root <root@ca9752122456>
2016-10-19 06:40:11 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "testjaxbimpl_prod",
+  "homePage" : "Main",
+  "name" : "testjaxbimpl_prod",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
